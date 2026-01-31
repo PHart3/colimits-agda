2bb7099bbb4b3cb8bd624415347ba404cb2f4451
PHart3 <perry.hart7@yahoo.com>
2026-01-31 09:42:56 -0600

    update sh script

--- agda-html.sh
+++ agda-html.sh
@@ -4,9 +4,9 @@ cd /build/Colimit-code
 /dist/agda --html --library-file=/dist/libraries /build/Colimit-code/Main-Theorem/CosColim-main.agda
 /dist/agda --html --library-file=/dist/libraries /build/Colimit-code/Create/Tree-preserve.agda
 /dist/agda --html --library-file=/dist/libraries /build/Colimit-code/Create/Tree-reflect.agda
+/dist/agda --html --library-file=/dist/libraries /build/Colimit-code/OFS-Preserve/CosColim-lftclass.agda
 
 cd /build/HoTT-Agda
-/dist/agda --html --library-file=/dist/libraries /build/HoTT-Agda/theorems/homotopy/ColimAdjoint-hex.agda
 /dist/agda --html --library-file=/dist/libraries /build/HoTT-Agda/theorems/homotopy/Acyc-colim.agda
 /dist/agda --html --library-file=/dist/libraries /build/HoTT-Agda/theorems/modality/Mod-colim.agda
 
